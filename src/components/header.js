0f2ec418e77f74b7e7dbc40fd5c0ee80ee3bd9b8
Shuhan Hu <1119han@gmail.com>
2020-03-10 21:04:55 +0800

    add navigation

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,26 @@
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+const Header = ({siteTitle}) => (
+  <header className="navigation">
+    <p className="navigation__logo">{siteTitle}</p>
+
+    <a href="#" className="navigation__item push-right">
+      Home
+    </a>
+    <a href="#" className="navigation__item">
+      Rails
+    </a>
+    <a href="#" className="navigation__item">
+      HTML
+    </a>
+    <a href="#" className="navigation__item">
+      CSS
+    </a>
+    <a href="#" className="navigation__item">
+      Git
+    </a>
   </header>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,12 +7,12 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import {useStaticQuery, graphql} from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "../css/index.sass"
 
-const Layout = ({ children }) => {
+const Layout = ({children}) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,21 +25,20 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle={data.site.siteMetadata.title} />{" "}
       <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
+        }}>
+        <main> {children} </main>{" "}
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+          {" "}
+          ©{new Date().getFullYear()}, Built with {` `}{" "}
+          <a href="https://www.gatsbyjs.org"> Gatsby </a>{" "}
+        </footer>{" "}
+      </div>{" "}
     </>
   )
 }
