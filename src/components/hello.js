3d90c2c1fa2cd2c322348888ab144467c01afb1a
Shuhan Hu <1119han@gmail.com>
2020-03-10 20:28:18 +0800

    add my first component

---
+++ src/components/hello.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Hello() {
+  return <h1> my first component </h1>
+}
+
+export default Hello
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Hello from "../components/hello"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people!</h1>
     <p>I am Rocketgirl, let's explore the universe of programming together.</p>
-
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <Hello />
+    <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
