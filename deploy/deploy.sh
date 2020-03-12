a1fe85f38842b3e95caabd4733a7f258dd3edae4
Shuhan Hu <1119han@gmail.com>
2020-03-12 22:13:23 +0800

    Get deployment script to work

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,13 @@ BUCKET_NAME="s3://rocketgirl-static-website"
 DISTRIBUTION_ID="E3LICOGBS3NUAZ"
 
 set -e # exit on error
+echo "Cleaning up"
 gatsby clean
+echo "Compiling bundle"
 gatsby build 
+echo "Uploading to S3"
 aws s3 sync public/ $BUCKET_NAME
+echo "Invalidating CloudFront cache"
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
+echo "Done"
+exit 0
--- src/markdown-pages/post-1.md
+++ src/markdown-pages/post-1.md
@@ -4,4 +4,8 @@ date: "2020-03-12"
 title: "Symbols in Ruby"
 ---
 
-hello world:DDDD
+不可當變數，而且萬一符號可以當變數，那 hash 就沒必要指定符號當 key 了
+
+**試著把符號當數字看**
+
+只要是一樣的 Symbol，就會有相同的.object_id
